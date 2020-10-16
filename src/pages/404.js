3771aed647c8fed821691defaa721e5bcc662883
yawei.lu <yawei.lu@bhex.io>
2020-10-16 19:52:46 +0800

    up

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,10 @@ import { Redirect } from "dva/router";
 import route_map from "../config/route_map";
 
 function IndexPage({ layout, dispatch, location }) {
+  const accounts = layout.store.accounts;
+  if (accounts.length) {
+    return <Redirect to={route_map.index} />;
+  }
   return <Redirect to={route_map.welcome} />;
 }
 
