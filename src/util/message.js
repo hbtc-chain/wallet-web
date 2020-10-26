b5ad0cf55408dec2919c09e558c6a9bbe98e0c00
yawei.lu <yawei.lu@bhex.io>
2020-10-26 12:50:57 +0800

    Up

--- src/util/message.js
+++ src/util/message.js
@@ -105,6 +105,7 @@ class MessageManager {
    * @param {*} obj
    */
   sendMessage({ id, type, data }) {
+    console.log(id, type);
     let obj = util.packmsg({
       from: CONST.MESSAGE_FROM_POPUP,
       to: CONST.MESSAGE_FROM_BACKGROUND,
